3612560834357b9a7c764286a94e9ae12e8faefc
Panakj <pankaj.csdeveloper@gmail.com>
2018-09-09 21:22:08 +0530

    files are added

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,8 @@
-const keys = require('../config/keys');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('../config/keys');
 
 passport.use(
     new GoogleStrategy({
@@ -9,11 +11,14 @@ passport.use(
         callbackURL: '/auth/google/callback'
         },
         (accessToken, profileToken, profile, done) => {
-            console.log(
-                'accessToken==>',accessToken,
-                'profileToken==>', profileToken,
-                'profile==>', profile,
-                'done==>', done);
+            new User({
+                googleID: profile.id
+            }).save();
+            // console.log(
+                // 'accessToken==>',accessToken,
+                // 'profileToken==>', profileToken,
+                // 'profile==>', profile,
+                // 'done==>', done);
             }
         )
     );
