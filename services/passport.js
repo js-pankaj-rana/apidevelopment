2176acbe1e5b1385bd8a6408f3572d9edd1dbd43
Pankaj Rana <panrana0@publicisgroupe.net>
2018-09-12 14:12:08 +0530

    Added files

--- services/passport.js
+++ services/passport.js
@@ -1,12 +1,6 @@
-const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-const app = express();
-
-const keys = require('./config/keys');
-
-const PORT =  process.env.PORT || 5000;   
+const keys = require('../config/keys');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -22,17 +16,3 @@ passport.use(new GoogleStrategy({
         }
     )
 );
-
-app.get(
-        '/auth/google',
-        passport.authenticate('google', {
-            scope: ['profile', 'email']
-        }
-    )
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
-
-console.log(PORT);
-app.listen(PORT);
