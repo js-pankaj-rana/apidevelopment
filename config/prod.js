7f86dcd9746c4f29a5e373faf4bcd55e7b9ea029
Pankaj Rana <panrana0@publicisgroupe.net>
2018-09-17 00:52:01 +0530

    facebook key added

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,8 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY 
+    cookieKey: process.env.COOKIE_KEY,
+    facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+    facebookClientSecret: process.env.FACEBOOK_SECRET,
 }
  
